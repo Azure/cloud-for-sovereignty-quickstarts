ac3fbf506ee69ca7d463151ac144b46dc9e99ad4
Lucas McDaniel <lmcdaniel@microsoft.com>
2024-06-21 08:55:39 -0700

    Adding PK to the employee ID column and changing hosting location.

--- sovereignApplications/confidential/contosoHR/data/schema.sql
+++ sovereignApplications/confidential/contosoHR/data/schema.sql
@@ -3,7 +3,7 @@
 --- SUMMARY: Create employee table ---
 CREATE TABLE [HR].[Employees]
 (
-    [EmployeeID] [int] IDENTITY(1,1) NOT NULL,
+    [EmployeeID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
     [SSN] [char](11) NOT NULL,
     [FirstName] [nvarchar](50) NOT NULL,
     [LastName] [nvarchar](50) NOT NULL,
